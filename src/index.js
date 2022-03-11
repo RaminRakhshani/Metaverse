c4bd5e4eb72372b75c526686fbc968c510100807
Ramin Rakhshani <RaminRakhshani@outlook.com>
2022-03-12 00:38:27 +0330

    add ground

--- src/index.js
+++ src/index.js
@@ -13,17 +13,16 @@ const dLight = new THREE.DirectionalLight(0xffffff, 0.5);
 light.add(dLight);
 scene.add(light);
 
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshPhongMaterial({ color: 0x00ff00 });
-// const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
-camera.position.z = 6;
+const geometry = new THREE.BoxGeometry(50, 0.1, 50);
+const material = new THREE.MeshPhongMaterial({ color: 0xffffff });
+const ground = new THREE.Mesh(geometry, material);
+
+scene.add(ground);
+camera.position.set(5, 15, 15);
 
 function animate() {
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
     requestAnimationFrame(animate);
+    camera.lookAt(ground.position);
     renderer.render(scene, camera);
 }
 animate();
