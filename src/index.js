0661c8d3c38c7a89ed05dcfe6829d9c8293b3945
Ramin Rakhshani <RaminRakhshani@outlook.com>
2022-03-12 00:27:54 +0330

    add geometry sample

--- src/index.js
+++ src/index.js
@@ -8,4 +8,10 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+camera.position.z = 6;
+
 renderer.render(scene, camera);
